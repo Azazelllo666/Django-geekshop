3df9669510b27c00f7b18f11dc6e8cc82fcc862a
Azazelllo666 <leonidovpi@yandex.ru>
2021-02-07 17:22:19 +0300

    Lesson_6

--- basket/urls.py
+++ basket/urls.py
@@ -6,4 +6,5 @@ app_name = 'baskets'
 urlpatterns = [
     path('basket-add/<int:product_id>/', basket.basket_add, name='basket_add'),
     path('basket-remove/<int:id>/', basket.basket_remove, name='basket_remove'),
+    path('edit/<int:id>/<int:quantity>/', basket.basket_edit, name='basket_edit'),
 ]
