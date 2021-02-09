4ce06015a1279e0b173a2a6d53681f1b85d31396
Azazelllo666 <leonidovpi@yandex.ru>
2021-02-09 22:14:45 +0300

    update project

---
+++ adminapp/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
+
+from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
+
+
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
+def index(request):
+    return render(request, 'adminapp/index.html')
+
+
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
+def admin_users_read(request):
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'adminapp/admin-users-read.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users_read'))
+    else:
+        form = UserAdminRegisterForm()
+    context = {
+        'title': 'GeekShop - Регистрация',
+        'form': form,
+    }
+    return render(request, 'adminapp/admin-users-create.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
+def admin_users_update(request, id):
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users_read'))
+    else:
+        form = UserAdminProfileForm(instance=user)
+    context = {
+        'form': form,
+        'current_user': user,
+    }
+    return render(request, 'adminapp/admin-users-update.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser, login_url='/')
+def admin_users_delete(request, id):
+    user = User.objects.get(id=id)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users_read'))
