f98183dbea01f425b8280e912bac050a334ece18
Azazelllo666 <leonidovpi@yandex.ru>
2021-03-09 21:45:57 +0300

    Создал UserProfile

--- authapp/models.py
+++ authapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 from datetime import timedelta
 
@@ -18,3 +20,27 @@ class User(AbstractUser):
             return False
         else:
             return True
+
+
+class UserProfile(models.Model):
+    MALE = "M"
+    FEMALE = "W"
+
+    GENDER_CHOICES = (
+        (MALE, "М"),
+        (FEMALE, "Ж"),
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    tagline = models.CharField(verbose_name="теги", max_length=128, blank=True)
+    aboutMe = models.TextField(verbose_name="о себе", max_length=512, blank=True)
+    gender = models.CharField(verbose_name="пол", max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
