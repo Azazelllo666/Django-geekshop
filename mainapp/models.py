ad0c3be81f91513ae97114e0e84f91a557f28b26
azazello <leonidovpi@yandex.ru>
2021-03-24 18:30:34 +0300

    lesson_14

--- mainapp/models.py
+++ mainapp/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    is_active = models.BooleanField(verbose_name="продукт активен", default=True)
+    is_active = models.BooleanField(verbose_name="продукт активен", db_index=True, default=True)
 
     def __str__(self):
         return f'{self.name} | {self.category.name}'
